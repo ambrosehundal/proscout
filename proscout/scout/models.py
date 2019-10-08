7fb6a6a7800754cfd96c4e9be7afd3fdc1811e46
Ambrose Hundal <amshunda@ucsc.edu>
2019-10-07 22:49:28 -0700

    model changes to athlete

--- proscout/scout/models.py
+++ proscout/scout/models.py
@@ -10,7 +10,9 @@ class Athlete(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField(default = 1)
     birth_date = models.CharField(max_length=20)
-    
+    hometown = models.CharField(max_length=40)
+    country = models.CharField(max_length=30)
+    primary_sport = models.CharField(max_length=20)
 
 
 
