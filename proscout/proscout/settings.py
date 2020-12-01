4bc7875fa789f9817d62f66ed3f94561feca841d
Ambrose Hundal <amshunda@ucsc.edu>
2020-11-30 20:45:58 -0800

    crispy forms added and registration form edited

--- proscout/proscout/settings.py
+++ proscout/proscout/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scout',
-    'registration'
+    'registration',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
--- proscout/registration/forms.py
+++ proscout/registration/forms.py
@@ -1,10 +1,16 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.models import models
+from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = models.EmailField()
+    email = forms.EmailField()
+    age = forms.IntegerField()
     
     class Meta:
+        model = User
+        fields = ["username", "email", "age", "password1", "password2"]
+     
+        
 
 
