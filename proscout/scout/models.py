0b97d1f849cfd59b473f333e1aeeae10f7295df4
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-01 23:19:21 -0800

    profile model changed and athlete model deleted

--- proscout/scout/models.py
+++ proscout/scout/models.py
@@ -5,19 +5,25 @@ from django.db import models
 
 # Create your models here.
 
-class Athlete(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField(default = 1)
-    birth_date = models.CharField(max_length=20)
-    hometown = models.CharField(max_length=40)
-    country = models.CharField(max_length=30)
+# class Athlete(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     age = models.IntegerField(default = 1)
+#     birth_date = models.CharField(max_length=20)
+#     hometown = models.CharField(max_length=40)
+#     country = models.CharField(max_length=30)
     
 
 
 
 
 class Profile(models.Model):
+    age = models.IntegerField(default = 1)
+    # birth_date = models.CharField(max_length=20)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    hometown = models.CharField(max_length=40)
+    country = models.CharField(max_length=30, null=True)
     headline = models.CharField(max_length=150)
     summary = models.CharField(max_length=200)
 
