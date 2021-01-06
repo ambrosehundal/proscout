8798ca71c1dcdd3b83cabad41efcda250f18e22a
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-06 10:45:14 -0800

    user and profile model relation created, and migrations done

--- proscout/scout/models.py
+++ proscout/scout/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 # class Athlete(models.Model):
@@ -18,14 +18,15 @@ from django.db import models
 
 
 class Profile(models.Model):
+    profile_user = models.ForeignKey(User, on_delete=models.CASCADE, default = 1)
     age = models.IntegerField(default = 1)
     # birth_date = models.CharField(max_length=20)
-    height = models.IntegerField()
-    weight = models.IntegerField()
-    hometown = models.CharField(max_length=40)
+    height = models.IntegerField(default = 72)
+    weight = models.IntegerField(default = 180)
+    hometown = models.CharField(max_length=40, blank=True)
     country = models.CharField(max_length=30, null=True)
-    headline = models.CharField(max_length=150)
-    summary = models.CharField(max_length=200)
+    headline = models.CharField(max_length=150, blank=True)
+    summary = models.CharField(max_length=200, blank=True)
 
 
 
