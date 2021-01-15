3d218c1c4f7d4bfe9aec88bb691888be85b22d31
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-14 22:20:49 -0800

    function  to save user profile fixed, works successfully

--- proscout/registration/models.py
+++ proscout/registration/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
-    friends = models.ManyToManyField("User", blank=True)
+# class User(AbstractUser):
+#     friends = models.ManyToManyField("User", blank=True)
 
 
-class FriendRequest(models.Model):
-    from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE())
-    to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE())
+# class FriendRequest(models.Model):
+#     from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE())
+#     to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE())
--- proscout/scout/views.py
+++ proscout/scout/views.py
@@ -11,13 +11,43 @@ from django.contrib.auth.forms import UserCreationForm
 
 from scout.forms import UserProfileForm
 
-
+from .models import Profile
 # Create your views here.
 
-
+# homepage
 def index(request):
     template = 'index.html'
     return render(request, template)
+
+
+
+# function for logged in user to create a new profile
+def new_profile(request):
+    current_user = request.user
+    print(current_user)
+    if request.method == "POST":	    
+        form = UserProfileForm(request.POST)	        
+        if form.is_valid():	        
+            user_profile = Profile()	            
+            user_profile.profile_user = current_user	          
+            user_profile.age = form.cleaned_data['age']	            
+            user_profile.height = form.cleaned_data['height']
+            user_profile.weight = form.cleaned_data['weight']
+            user_profile.country = form.cleaned_data['country']
+            user_profile.disciplines = form.cleaned_data['disciplines']
+            user_profile.headline = form.cleaned_data['headline']
+            user_profile.summary = form.cleaned_data['summary']
+            user_profile.save()	            
+            return redirect("/")
+        else:
+            form = UserProfileForm()
+
+    return render(response, "homepage.html", {"form":form})
+
+
+
+
+    
    
 
 
@@ -32,12 +62,6 @@ def profile_homepage(request):
 
 
 
-def create_profile(request):
-
-    if request.method == "POST":
-        print("yo")
-
-    return HttpResponseRedirect('/index')
 
             
 
