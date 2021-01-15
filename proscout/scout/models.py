f3d9e1a293aa1074864d73c7d86dc82c9dd604b4
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-14 22:49:47 -0800

    profile model schema changed

--- proscout/scout/models.py
+++ proscout/scout/models.py
@@ -5,22 +5,16 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-# class Athlete(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50)
-#     age = models.IntegerField(default = 1)
-#     birth_date = models.CharField(max_length=20)
-#     hometown = models.CharField(max_length=40)
-#     country = models.CharField(max_length=30)
-    
+
 
 
 
 
 class Profile(models.Model):
-    profile_user = models.ForeignKey(User, on_delete=models.CASCADE, default = 1)
-    age = models.IntegerField(default = 1)
-    # birth_date = models.CharField(max_length=20)
+    profile_user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    first_name = models.CharField(max_length=50, default="Conor")
+    last_name = models.CharField(max_length=50, default="Mcgregor")
+    birth_date = models.DateField()
     height = models.IntegerField(default = 72)
     weight = models.IntegerField(default = 180)
     hometown = models.CharField(max_length=40, blank=True)
