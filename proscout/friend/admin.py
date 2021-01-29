5d5c55518477ab54ab7974e21081a8678fb88c2c
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-28 21:28:09 -0800

    friend request admin added

--- proscout/friend/admin.py
+++ proscout/friend/admin.py
@@ -17,3 +17,17 @@ class FriendListAdmin(admin.ModelAdmin):
 
 
 admin.site.register(FriendList, FriendListAdmin)
+
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver']
+    search_fields = ['sender__username', 'sender__email' 'receiver__username', 'receiver__email']
+
+
+    class Meta:
+        model = FriendRequest
+
+admin.site.register(FriendRequest, FriendRequestAdmin)
+
+
