965bda3793c48854e66e9c0159f9252e4fe48879
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-31 15:52:59 -0800

    added links to homepage

--- proscout/scout/views.py
+++ proscout/scout/views.py
@@ -82,7 +82,10 @@ def profile_homepage(request):
 
 def mma_subreddit(request):
     mma_news_template = 'mma_news.html'
-    
+    reddit = praw.Reddit(client_id='', \
+                     client_secret='', \
+                     user_agent='mma', \
+                    )  
 
     mma_subreddit = reddit.subreddit("mma").hot(limit=15)
 
