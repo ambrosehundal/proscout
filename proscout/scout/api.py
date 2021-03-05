bc3e0882e15d5307931c468414ef1baf6da1a010
Ambrose Hundal <amshunda@ucsc.edu>
2021-03-04 21:02:33 -0800

    user profile page added

--- proscout/scout/api.py
+++ proscout/scout/api.py
@@ -1,17 +1,22 @@
 from rest_framework import generics
-from rest_framework.response import Response 
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 from .serializers import ProfileSerializer
 from .models import Profile
 from django.contrib.auth.models import User
 
 class ProfileView(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    
-    def get_queryset(self):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'homepage.html'
+
+
+    def get(self, request, username=None):
 
         username = self.kwargs['username']
 
         user_id = User.objects.values_list('id', flat=True).get(username=username)
 
-        return Profile.objects.filter(user=user_id)
+        user_profile = Profile.objects.filter(user=user_id)
+        return Response({'profile':user_profile})
 
