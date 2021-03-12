5824bc18b6a5d08f51687cbc33b94611ac7a144f
Ambrose Hundal <amshunda@ucsc.edu>
2021-03-11 21:33:53 -0800

    added logic to allow user to edit their profile

--- proscout/scout/api.py
+++ proscout/scout/api.py
@@ -4,6 +4,7 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from .serializers import ProfileSerializer
 from .models import Profile
 from django.contrib.auth.models import User
+from scout.forms import UserProfileForm
 
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
@@ -14,8 +15,15 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     def get(self, request, username=None):
 
         if request.user.is_authenticated:
-        # 
-        # add edit model instance form    
+            user_id = User.objects.values_list('id', flat=True).get(username=request.user)
+            user_profile = Profile.objects.filter(user=user_id)
+
+            profile_edit_form = UserProfileForm(instance=user_profile)
+
+            return Response({'profile':user_profile, 'edit_form':profile_edit_form})
+        #   
+        # add edit model instance form   
+
         
 
         username = self.kwargs['username']
